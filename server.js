a95401326f8352993a70ef7d1d9c9347054e55df
cansuyavuz2 <cansu-yavuz23@hotmail.de>
2024-05-21 20:38:38 +0200

    test github actions

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps FS2024 :-)))');
+  res.send('Hello DevOps FS2024 :-))) Deployed from githubactions to Azure');
 });
 
 app.listen(PORT, HOST);
